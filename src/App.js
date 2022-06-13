ea3d092b3a98cdf8f1c77a0fd69a6eaabf1c30df
michael o'donovan <mikeodonovan3@gmail.com>
2022-06-13 17:22:03 +0100

    feat: add Exported Data section

--- src/App.js
+++ src/App.js
@@ -20,70 +20,65 @@ export default function App() {
 
   return (
 
-    <>
+    <form onSubmit={handleSubmit((data) => {
+      console.log(data)
+      setFormData(data)
+    })}>
 
+      <h3>Mock up of form with set country list</h3>
 
-      <form onSubmit={handleSubmit((data) => {
-        console.log(data)
-        setFormData(data)
-      })}>
+      <div className="field-wrapper">
+        <label>first name:</label>
+        <input {...register("firstName", { required: true })} placeholder=" required"/>
+      </div>
+      {errors.firstName && <span>This field is required</span>}
 
-        <h3>Mock Up of form with set country list</h3>
+      <div className="field-wrapper">
+        <label>second name:</label>
+        <input {...register("secondName", { required: true })} placeholder=" required"/>
+      </div>
+      {errors.secondName && <span>This field is required</span>}
 
-        <div className="field-wrapper">
-          <label>first name:</label>
-          <input {...register("firstName", { required: true })} placeholder=" required"/>
-        </div>
-        {errors.firstName && <span>This field is required</span>}
+      <div className="field-wrapper">
+        <label>address line 1</label>
+        <input {...register("addressLine1", { required: true })} placeholder=" required"/>
+      </div>
+      {errors.addressLine1 && <span>This field is required</span>}
 
-        <div className="field-wrapper">
-          <label>second name:</label>
-          <input {...register("secondName", { required: true })} placeholder=" required"/>
-        </div>
-        {errors.secondName && <span>This field is required</span>}
+      <div className="field-wrapper">
+        <label>address line 2</label>
+        <input {...register("addressLine2", { required: true })} placeholder=" required"/>
+      </div>
+      {errors.addressLine2 && <span>This field is required</span>}
 
-        <div className="field-wrapper">
-          <label>address line 1</label>
-          <input {...register("addressLine1", { required: true })} placeholder=" required"/>
-        </div>
-        {errors.addressLine1 && <span>This field is required</span>}
+      <div className="field-wrapper">
+        <label>address line 3</label>
+        <input {...register("addressLine3")} placeholder=" optional"/>
+      </div>
 
-        <div className="field-wrapper">
-          <label>address line 2</label>
-          <input {...register("addressLine2", { required: true })} placeholder=" required"/>
-        </div>
-        {errors.addressLine2 && <span>This field is required</span>}
+      <div className="field-wrapper">
+        <label>postcode:</label>
+        <input {...register("postcode", { required: true })} placeholder=" required"/>
+      </div>
+      {errors.postcode && <span>This field is required</span>}
 
-        <div className="field-wrapper">
-          <label>address line 3</label>
-          <input {...register("addressLine3")} placeholder=" optional"/>
-        </div>
+      <div className="field-wrapper">
+        <label>country:</label>
+        <Select options={countryOptions} value={country} onChange={changeCountryHandler}/>
+      </div>
 
-        <div className="field-wrapper">
-          <label>postcode:</label>
-          <input {...register("postcode", { required: true })} placeholder=" required"/>
-        </div>
-        {errors.postcode && <span>This field is required</span>}
+      <input className="submit-button" type="submit" value="submit"/>
 
-        <div className="field-wrapper">
-          <label>country:</label>
-          <Select options={countryOptions} value={country} onChange={changeCountryHandler}/>
-        </div>
+      <h3>Exported Data</h3>
+      <div>{formData.firstName}</div>
+      <div>{formData.secondName}</div>
+      <div>{formData.addressLine1}</div>
+      <div>{formData.addressLine2}</div>
+      <div>{formData.addressLine3}</div>
+      <div>{formData.postcode}</div>
+      <div>{formData.country}</div>
 
-        <input className="submit-button" type="submit" value="submit"/>
-
-        <h3>Exported Data</h3>
-        <div>{formData.firstName}</div>
-        <div>{formData.secondName}</div>
-        <div>{formData.addressLine1}</div>
-        <div>{formData.addressLine2}</div>
-        <div>{formData.addressLine3}</div>
-        <div>{formData.postcode}</div>
-        <div>{formData.country}</div>
-
-      </form>
-
-    </>
+    </form>
 
   );
 }
